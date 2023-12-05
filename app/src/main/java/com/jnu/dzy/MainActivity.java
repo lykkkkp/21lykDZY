39d9246cba5f52a464c5173828096e6fa588614d
lykkkkp <250205846@qq.com>
2023-12-05 17:55:30 +0800

    2任务框架

--- app/src/main/java/com/jnu/dzy/MainActivity.java
+++ app/src/main/java/com/jnu/dzy/MainActivity.java
@@ -7,16 +7,25 @@ import android.view.Menu;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.jnu.dzy.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private String[]titles={"新建","每日任务","每周任务","普通任务","已完成"};
+    private TabLayout tab_layout;
+    private ViewPager2 viewPager;
 
 
 
@@ -25,6 +34,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tab_layout=findViewById(R.id.tab_layout);
+        viewPager=findViewById(R.id.viewPager);
+
+        viewPager.setAdapter(new FragmentStateAdapter(this){
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                String title = titles[position];
+                TabNewsFragment tabNewsFragment = TabNewsFragment.newInstance(title);
+                return tabNewsFragment;
+            }
+
+            @Override
+            public int getItemCount() {
+                return titles.length;
+            }
+        });
+
+
+        //点击事件
+        tab_layout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition(),false);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        TabLayoutMediator tabLayoutMediator=new TabLayoutMediator(tab_layout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(titles[position]);
+            }
+        });
+
+        tabLayoutMediator.attach();
     }
 
 }
---
+++ app/src/main/java/com/jnu/dzy/TabNewsFragment.java
@@ -0,0 +1,57 @@
+package com.jnu.dzy;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kotlin.text.UStringsKt;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TabNewsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TabNewsFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM = "title";
+    private String title;
+
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public TabNewsFragment() {
+        // Required empty public constructor
+    }
+
+    public static TabNewsFragment newInstance(String param) {
+        TabNewsFragment fragment = new TabNewsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM, param);
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            title = getArguments().getString(ARG_PARAM);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_news, container, false);
+    }
+}
